8154cef1c5bc034fbf8bd26104a2e0c25aaf6399
郭敬文 <guojingwen@dodoca.com>
2016-10-10 10:01:49 +0800

    init

--- smartDemo/app.js
+++ smartDemo/app.js
@@ -2,7 +2,6 @@
 
 var Koa = require('koa');
 var path = require('path');
-var toStatic = require('koa-static')
 var route = require('koa-route');
 var wechat = require('./wechat/g');
 var config = require('./config');
@@ -11,31 +10,13 @@ var weixin = require('./weixin');
 
 var app = new Koa();
 
-/*app.use(function* (next) {
-    // TODO  这里做拦截,权限验证
-    if (this.path === '/wechat') {
-        wechat(config.wechat,weixin.reply);
-        return next
-
-    }
-    yield next;
-})*/
-
-
-/*function *register(){
-    this.body = 'register';
-}
-app.use(route.get('/register', register));*/
-// app.use(route.get('/wechat', wechat(config.wechat,weixin.reply)));
-// app.use(route.post('/wechat', wechat(config.wechat,weixin.reply)));
-
 app.use(wechat(config.wechat, weixin.reply));
-
-//app.use(toStatic(__dirname + '/static'));
-
 
 app.listen(1244);
 console.log("Listening: 1244 ");
 
+
+//var toStatic = require('koa-static')
+//app.use(toStatic(__dirname + '/static'));
 // node --harmony app.js //开启服务
 // ./ngrok -config ngrok.cfg -subdomain scott_wechat 1234
